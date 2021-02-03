f89e2cfdf98b543c26b290ce06a4505a2a155791
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-02-03 15:23:48 -0500

    Initialize Reverse Consumer

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from file_iterator import FileIterator
 
 if __name__ == '__main__':
     amplify_consumer = consumers.AmplifyConsumer(amplifier=1000)
-    iterator = FileIterator("test", amplify_consumer)
+    reverse_consumer = consumers.ReverseConsumer()
+    iterator = FileIterator("test", reverse_consumer)
     iterator.iterate_all()

1ca12b01789884a1a31f7e1c7dca539b33145318
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-10-29 19:31:54 -0400

    Add MonoConsumer to main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from file_iterator import FileIterator
 if __name__ == '__main__':
     amplify_consumer = consumers.AmplifyConsumer(amplifier=1000)
     reverse_consumer = consumers.ReverseConsumer()
-    append_consumer = consumers.AppendRandom("other", volume=-6)
-    iterator = FileIterator("test", append_consumer)
+    append_consumer = consumers.AppendRandom("other", volume=-12)
+    mono_consumer = consumers.MonoConsumer()
+    iterator = FileIterator("test", mono_consumer)
     iterator.iterate_all()
